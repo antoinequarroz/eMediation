675c43aaa2e22c58e2443017041260c0e1dccf0d
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2021-02-03 00:12:02 +0100

    Mise à jour de l'image d'accueil

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -22,7 +22,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('EMediation');
+            ->setTitle('eMediation');
     }
 
     public function configureMenuItems(): iterable
