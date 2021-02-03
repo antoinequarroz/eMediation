f09af9b8bd983b0c906efbb3154bcdfd316dd670
Quarroz Antoine <antoinequarroz.mediam@gmail.com>
2021-02-03 10:02:00 +0100

    Finition du ProductCrudController

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -58,6 +58,11 @@ class Product
      */
     private $category;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -155,6 +160,18 @@ class Product
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
